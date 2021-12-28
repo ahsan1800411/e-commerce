a1dbeba18a41f41dc6fcb54fb25d69f45a362db2
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-12-28 23:17:50 +0500

    Initial Commit

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,3 +1,5 @@
+const ErrorHandler = require('../utils/errorHandler');
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal Server Error';
@@ -7,7 +9,7 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
     res.status(err.statusCode).json({
       success: false,
-      statusCode: err.statusCode,
+      error: err,
       message: err.message,
       stack: err.stack,
     });
@@ -15,9 +17,25 @@ module.exports = (err, req, res, next) => {
 
   // Production Errors
   if (process.env.NODE_ENV === 'PRODUCTION') {
+    // Wrong Mongoose Object Id Error;
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid ${err.path}`;
+      err = new ErrorHandler(message, 400);
+    }
+
+    // Validation Errors;
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((d) => d.message);
+      err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: err.message,
     });
   }
 };
+
+// Object.values();
+// Object.keys();
+// Object.entries()
