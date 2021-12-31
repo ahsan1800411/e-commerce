5cab90a412aa534b435f3722107a079976848e2f
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-12-31 23:26:02 +0500

    Initial Commit

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,9 @@
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
 // register User ==> Post Request >> /api/v1/register;
 
 exports.register = catchAsyncErrors(async (req, res, next) => {
@@ -8,15 +11,21 @@ exports.register = catchAsyncErrors(async (req, res, next) => {
 
   // Checks if the user is alredy in the database
   const emailExist = await User.findOne({ email });
-  if (emailExist)
-    return res
-      .status(400)
-      .json({ succes: false, message: 'Email already existed ' });
+  if (emailExist) return next(new ErrorHandler(`Email already exists`, 400));
+  const isValidPassword = validator.isLength(password, { min: 6 });
+  if (!isValidPassword) {
+    return next(
+      new ErrorHandler(`Password must be at least 6 characters long`, 400)
+    );
+  }
+
+  // Encrypting a password;
+  const hashPassword = await bcrypt.hash(password, 10);
 
   let newUser = await User.create({
     name,
     email,
-    password,
+    password: hashPassword,
     avatar: {
       public_id: 'Hi, There',
       url: 'https://hfmfnhfngmg',
--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please enter your name'],
-    maxLength: [30, 'Your name cannot exceed 30 characters'],
+    maxlength: [30, 'Your name cannot exceed 30 characters'],
   },
   email: {
     type: String,
