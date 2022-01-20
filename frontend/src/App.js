3db9653d6f795cb75945a9219aee79132cab586d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2022-01-20 22:55:28 +0500

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import Home from './components/layouts/Home';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class='py-1'>
+      <p class='text-center  mt-1'>
+        Shopping Cart - 2022-2023, All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
